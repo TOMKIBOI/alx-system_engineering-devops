58156993218e9211bea91b86aef08a2436527daa
TOMKIBOI <tomkiboi@yahoo.com>
2024-01-24 17:53:09 -0800

    flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-# using puppet to install flask from pip3
+# install_flask.pp
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => '/usr/local/bin',
+  require => Package['python3-pip'],
 }
