8c776fe5bcb78e690699e52d102939a2e532eb32
TTom Kiboi <tomkisira@gmail.com>
2024-06-20 14:11:36 -0700

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,38 @@
 
 #!/usr/bin/python3
 """
-Script that queries subscribers on a given subreddit.
+a function that queries the Reddit API and returns the number of subscribers
 """
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    """
+    Returns the number of subscribers for a given subreddit.
+
+    Args:
+        subreddit (str): The subreddit to search.
+
+    Returns:
+        int: Number of subscribers of the subreddit.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'MyBot/0.0.1'}
+
+    response = requests.get(url, headers=headers)
+
     if response.status_code == 200:
         data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
+        return data['data']['subscribers']
     else:
         return 0
+
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        subreddit = sys.argv[1]
+        print(number_of_subscribers(subreddit))
